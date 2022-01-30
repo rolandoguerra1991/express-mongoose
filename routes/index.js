eccc863b492c25d4807b07c7dc93c5e70b429b3c
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-01-30 14:36:52 -0400

    Dyanmic Routing

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
-const authRoutes = require('./auth.router');
-const usersRoutes = require('./users.router');
-
 const router = express.Router();
+const fs = require('fs');
 
-router.use('/auth', authRoutes);
-router.use('/users', usersRoutes);
-
+// Dyanmic Routing
+// Adds all routes from routes folder
+fs.readdir('./routes', (err, files) => {
+  files.forEach((file) => {
+    if (file !== 'index.js') {
+      const url = `/${file.split('.')[0]}/`;
+      const route = `./${file}`;
+      router.use(url, require(route));
+    }
+  });
+});
 
 module.exports = router;
