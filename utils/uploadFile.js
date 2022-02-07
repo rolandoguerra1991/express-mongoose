7c1087dd9be3c57c7184c568158f074f83869d69
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-02-07 18:16:53 -0400

    multer

---
+++ utils/uploadFile.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../public/uploads'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
+    cb(null, uniqueSuffix)
+  }
+});
+
+module.exports = multer({ storage: storage });
