ac69bced317cae387db1e0eb1684f4b881657bb4
Rolando Guerra <rolandoguerra1991@gmail.com>
2022-03-17 12:22:41 -0400

    fix multer folder

--- src/utils/uploadFile.js
+++ src/utils/uploadFile.js
@@ -4,7 +4,7 @@ const path = require('path')
 const upload = (folder) => {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, `../uploads/${folder}/`))
+      cb(null, path.join(__dirname, `../../uploads/${folder}/`))
     },
     filename: (req, file, cb) => {
       const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`
